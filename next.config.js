f31f0dd4f0c14b33b82bb6c3c9bafa3ec3816076
André Fernandes <andrefersouza@gmail.com>
2023-03-01 16:20:28 -0300

    correção da build de deploy para o Github

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
-const repo = 'fernandes_nutri'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
+  /**
+   * Tell Next.js where the `public` folder is.
+   * Replace `nextjs-github-pages` with your Github repo project name.
+   */
+  assetPrefix: isProd ? "/fernandes_nutri/" : "",
+  /**
+   * Disable server-based image optimization.
+   *
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
+  images: {
+    unoptimized: true,
+  },
+};
